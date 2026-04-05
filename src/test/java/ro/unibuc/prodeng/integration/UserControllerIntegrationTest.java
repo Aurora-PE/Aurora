abdc3530893b8668fcade53abffa56078b4dd8fe
Stinga Alexandru-Ionut <147251716+DistroZLeon@users.noreply.github.com>
2026-04-05 18:05:59 +0000

    [CHORE] Triggering First CI Build

--- src/test/java/ro/unibuc/prodeng/integration/UserControllerIntegrationTest.java
+++ src/test/java/ro/unibuc/prodeng/integration/UserControllerIntegrationTest.java
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+// Triggering CI build 1
 @DisplayName("UserController Integration Tests")
 class UserControllerIntegrationTest extends IntegrationTestBase {
 
